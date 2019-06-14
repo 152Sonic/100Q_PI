e3085514af6e152d9af5f8d8d8ca3b491ebc8ab3
152Sonic <51821291+152Sonic@users.noreply.github.com>
2019-06-14 19:00:20 +0100

    Create questão_9.c

---
+++ questão_9.c
@@ -0,0 +1,11 @@
+/*
+  Apresente uma definição da função pré-definida em C "int strcmp (char s1[], char s2[])"
+  que compara (lexicograficamente) duas strings. O resultado deverá ser:
+   • 0, se as strings forem iguais;
+   • <0, se s1<s2;
+   • >0, se s1>s2.
+*/
+int mystrcmp(char s1[], char s2[]) {
+    // adicione aqui o seu código
+    return 0;
+}
