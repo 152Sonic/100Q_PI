2ee1b360f013effbf7582273050276b3768f5801
152Sonic <51821291+152Sonic@users.noreply.github.com>
2019-06-14 20:56:20 +0100

    Create questão13.c

---
+++ questão13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/* Defina uma função 
+   void truncW (char t[], int n) 
+   que dado um texto t com várias palavras 
+   (as palavras estão separadas por um ou 
+   mais espaços) e um inteiro n, trunca 
+   todas as palavras de forma a terem no 
+   máximo n caracteres.
+*/
+
+void truncW (char t[], int n){
