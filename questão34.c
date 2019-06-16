8312d9f83a0fb78cdb911bc738eaf2c387b1f7e5
152Sonic <51821291+152Sonic@users.noreply.github.com>
2019-06-16 23:45:36 +0100

    Create questão34.c

---
+++ questão34.c
@@ -0,0 +1,14 @@
+   int elimRepOrd (int v[], int N) {
+       int i,j=0,n;
+       for (i=0;i<N;i++)
+       {
+           if (v[i]!=v[j])
+           {
+               j++;
+               v[j]=v[i];
+           }
+       }
+       n=j+1;
+       
+       return n;
+   }
