390d9653bbb7af20d4ff84eefaaea2bb5ce2d30f
152Sonic <51821291+152Sonic@users.noreply.github.com>
2019-06-17 11:23:08 +0100

    Create questão49.c

---
+++ questão49.c
@@ -0,0 +1,17 @@
+#include "maiscentral.h"
+
+int distancia (Posicao n){
+    return (abs (n.x) + abs (n.y));
+}
+
+int maiscentral (Posicao pos[], int N) {
+    
+    int i,n=0, min=distancia (pos[0]);
+    
+    for(i=0;i<N;i++)
+    {
+       if (distancia (pos[i])<min) {min=distancia (pos[i]);n=i;}
+    }
+    
+    return n;
+}
