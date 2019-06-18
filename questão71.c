1be3772742e7220687543c5924f193be1f755118
152Sonic <51821291+152Sonic@users.noreply.github.com>
2019-06-18 11:26:29 +0100

    Create questão71.c

---
+++ questão71.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listas.h"
+
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+LInt NForward (LInt l, int N){
+    for (N;N!=0;N--) l=l->prox;
+    return l;
+}
